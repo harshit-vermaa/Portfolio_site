f5b6c325bdfbd040603862a526e330404632be22
Harshit Verma <harshitvermaofficial59@gmail.com>
2026-05-11 16:43:56 +0100

    Fix: Set portfolio_7 as the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Portfolio_2 from "./portfolio-2";
-import Portfolio_3 from "./portfolio_3";
-import Portfolio_4 from "./portfolio_4";
-import Portfolio_5 from "./portfolio_5";
-import Portfolio_6 from "./portfolio_6";
-import Portfolio from "./portfolio";
-import Portfolio_7 from "./portfolio_7"
+
+import Portfolio from "./portfolio_7"
 export default function Page() {
-  return <Portfolio_7 />;
+  return <main>
+      <Portfolio />
+    </main>;
 }
